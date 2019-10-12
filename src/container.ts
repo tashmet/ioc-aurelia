d96545e7536aa42dced8652fbf788c9237438e1b
Björn Andersson <bjorn.olof.andersson@gmail.com>
2019-10-12 14:22:04 +0200

    Updated dependencies and removed integration tests

--- src/container.ts
+++ src/container.ts
@@ -6,13 +6,13 @@ export class AureliaAdapter extends AbstractContainer {
     private container: AureliaContainer
   ) { super(); }
 
-  public get<T>(req: ServiceRequest<T>): T {
-    return req instanceof Resolver ? req.get(this) : this.container.get(req);
+  protected get<T>(req: ServiceIdentifier<T>): T {
+    return this.container.get(req);
   }
 
   public registerResolver<T>(key: ServiceIdentifier<T>, resolver: Resolver<T>) {
     this.container.unregister(key);
-    this.container.registerHandler(key, () => resolver.get(this));
+    this.container.registerHandler(key, () => resolver.resolve(this));
   }
 
   public isRegistered<T>(key: ServiceRequest<T>): boolean {
